845deda1c4991062ba16950bdd706fa9b3d88067
rchen-yototec <rchen@yototec.com>
2025-03-29 19:32:48 +0000

    Update __init__.py

--- aparecium/__init__.py
+++ aparecium/__init__.py
@@ -1,14 +1,1 @@
-__version__ = "0.0.1"
-
-
-def reveal(embedding_vector):
-    """
-    Placeholder function that will reveal text from embedding vectors in future versions.
-
-    Args:
-        embedding_vector: A vector representation of embedded text
-
-    Returns:
-        str: Currently returns a placeholder message
-    """
-    return "Text revelation capability coming soon!"
+__version__ = "0.1.0"
